81240709871d2f2a6d5ce9da55bc64425bf18d71
Gowtham <gowtham.079@gmail.com>
2019-12-01 22:59:50 +0530

    Text box changes

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import './search-box.styles.css'
 
 export const SearchBox = ({placeholder, handleChange}) => (
-    <input 
-    className="search"
-    type="search" 
-    placeholder={placeholder}
-    onChange={handleChange}
-    />
+
+    <div className="sample ten">
+        <input 
+      className="search"
+      type="search" 
+      placeholder={placeholder}
+      onChange={handleChange}
+         />
+     <button type="submit" className="btn btn-search">
+         <i className="fa fa-search"></i>
+        </button>
+         <button type="reset" className="btn btn-reset fa fa-times"></button>
+    </div>
 );
