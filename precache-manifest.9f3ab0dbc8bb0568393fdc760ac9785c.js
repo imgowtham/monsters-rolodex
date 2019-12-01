d75d27c5df24ee0dc71d31b5b6e51d5322902e99
Gowtham <gowtham.079@gmail.com>
2019-12-01 22:13:50 +0530

    Updates

---
+++ precache-manifest.9f3ab0dbc8bb0568393fdc760ac9785c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8c5b2a098ebdb121b672c26ed35f12",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "59e3d58e4e7b21ded481",
+    "url": "/monsters-rolodex/static/css/main.dc55e971.chunk.css"
+  },
+  {
+    "revision": "6ef49256656a9eb2de46",
+    "url": "/monsters-rolodex/static/js/2.8629efad.chunk.js"
+  },
+  {
+    "revision": "59e3d58e4e7b21ded481",
+    "url": "/monsters-rolodex/static/js/main.a24ba77f.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
